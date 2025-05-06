a3ac300ba386c99cfa0a462c3aa5f0d62395a437
Aryan095 <aryanshukla095@gmail.com>
2025-05-07 01:06:20 +0530

    feat: scaffold React+Tailwind and Hello Samvad

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+ import React from 'react'
+ 
+ const App = () => {
+   return (
+     
+     <div className='h-screen flex items-center justify-center bg-gray-50'>
+     <h1 className='text-4xl font-bold text-indigo-600'> Samvad</h1>
+     </div>
+   )
+ }
+ 
+ export default App
