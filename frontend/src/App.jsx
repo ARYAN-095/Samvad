d741c12f16015d55c59d685312fb7ee2a5bd4fce
Aryan095 <aryanshukla095@gmail.com>
2025-05-07 01:15:52 +0530

    chore: add ESLint & Prettier config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
- import React from 'react'
- 
- const App = () => {
-   return (
-     
-     <div className='h-screen flex items-center justify-center bg-gray-50'>
-     <h1 className='text-4xl font-bold text-indigo-600'> Samvad</h1>
-     </div>
-   )
- }
- 
- export default App
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold text-indigo-600"> Samvad</h1>
+    </div>
+  );
+};
+
+export default App;
